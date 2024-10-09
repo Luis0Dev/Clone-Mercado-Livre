e58c2be88e4fb381d76c09332e554e8968cccb40
Luis0DEV <luisclaudionucci@gmail.com>
2024-10-09 20:41:17 -0300

    start_project

---
+++ src/main/java/com/Luis0DEV/MercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package com.Luis0DEV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLivreApplication.class, args);
+	}
+
+}
